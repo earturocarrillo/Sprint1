6553e0a8bde9a9d24eba91ce552747635379e950
Edwar Arturo Carrillo <arthurkarrillo@gmail.com>
2020-12-16 22:56:21 -0500

    actualizacion

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=b228a6ca3c764188a00d05fdf4cf0a55';
-var req = await new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b228a6ca3c764188a00d05fdf4cf0a55';
+
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
+.cath(err=>console.log())
